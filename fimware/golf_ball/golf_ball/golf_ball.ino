0708e39cd8b4a30eac5c92ffbdc43cc020d93b54
galjakub <44644846+JakubGal@users.noreply.github.com>
2025-03-08 00:54:44 +0100

    production_upgrade

--- fimware/golf_ball/golf_ball/golf_ball.ino
+++ fimware/golf_ball/golf_ball/golf_ball.ino
@@ -1,6 +1,7 @@
 long adc;
 int loop_count = 0;
 void setup() {
+  delay(100);
   pinMode(2, OUTPUT); //LED
   pinMode(3, OUTPUT); //MOSFET
   pinMode(4, INPUT); //ADC
@@ -20,16 +21,18 @@ void loop() {
   //Takes aproximatli100uS
 
   adc += analogRead(4);
+  
   if (loop_count == 1000) {
     adc = adc / 1000;
 
-    if (adc < 660) {
+    if (adc < 758) {  //3.65V
       digitalWriteFast(2, HIGH);
-      if (adc < 530) {
+      if (adc < 632) {  //3V Lowest safe voltage for lion+-
         while (true) {
           digitalWriteFast(2, HIGH);
           delay(100);
           digitalWriteFast(2, LOW);
+          
           delay(1000);
         }
       }
